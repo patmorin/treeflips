5e31da1511c02acdbf1959850de069846126781f
Pat Morin <morin@scs.carleton.ca>
2018-12-07 14:01:57 -0500

    Pffft

--- treeflips.py
+++ treeflips.py
@@ -1,46 +1,11 @@
 #!/usr/bin/python3
 """ Small program for testing a conjecture/question about local algorithms
     for computing the MST of a random point set.
-
-    Let T be a path of size n and assign a random point in [0,1]^2
-    to each vertex of T.  Now, select a random path abc in T of size 3
-    and, if |ab| > |ac| or |bc| > |ac| then replace the longer of those
-    two edges with the edge ac.  The process finishes when this is no longer
-    possible, so that, for every size 3 path abc, |ac| is the longest edge
-    of the triangle abc.
-
-    Conclusion: This algorithm does not produce very good trees.  It seems that
-    the resulting spanning tree has total length approximately .38n whereas the
-    MST has total length O(sqrt(n)).  Is there a simple rigorous proof that the
-    resulting tree was total length Omega(n)?
-
-    Two possible generalizations:
-
-    1. Select two random vertices u and v of T whose distance is at most k and
-       consider the path u=x_1,...,x_r=v in T.  Consider doing any improvement
-       that involves removing somme edge x_ix_{i+1} and replacing it with the
-       closest pair ab with a in {x_1,...,x_i} and b in {x_{i+1},...,x_r}.
-
-    2. Select a random connected subtree T' of T of size k and replace the edges
-       of T' with the MST of V(T').
-
-    There is a reasonable probability that one or both of these algorithms
-    produces a tree of expected length O(sqrt(n)) for a sufficiently large k.
-    Maybe k >= c*log n is sufficient?
-
-    The second algorithm obviously produces the MST if k=n.  Slightly less
-    obviously, the first algorithm does also:  If uv is an edge of the MST 
-    that it not already in T, then the path from u to v in T contains at least
-    one edge of length greater than |uv|.
 """
 import sys
 import random
 import itertools
-import functools
 import math
-
-
-
 
 """ Make a path on the vertex set {0,...,n-1} """
 def make_path(n):
@@ -65,7 +30,6 @@ def slideable_2paths(neighbours, points):
     paths = list(itertools.chain.from_iterable(paths))
     return [ p for p in paths if is_slideable([points[x] for x in p]) ]
 
-
 """ Return the squared distance between points a and b """
 def distance_sq(a, b):
     return (a[0]-b[0])**2 + (a[1]-b[1])**2
@@ -80,7 +44,7 @@ def longest_edge(cycle):
     m = max(d)
     return d.index(m)
 
-""" Return true iff replacing """
+""" Return true iff the longest edge of cycle is the not the edge c[-1]c[0] """
 def is_slideable(cycle):
     return longest_edge(cycle) != len(cycle)-1
 
@@ -92,7 +56,7 @@ def slide_edge(neighbours, path):
     neighbours[i].add(k)
     neighbours[k].add(i)
 
-""" Compute the total (squared) lenght of the graph """
+""" Compute the total (squared) length of the graph """
 def total_length(neighbours, points):
     length = 0
     for i in range(len(neighbours)):
@@ -115,24 +79,16 @@ if __name__ == "__main__":
         n = int(sys.argv[1])
     points = [(random.random(), random.random()) for _ in range(n)]
     neighbours = make_path(n)
-    #print(points)
-    #print(neighbours)
     print(total_length(neighbours, points))
     paths = slideable_2paths(neighbours, points)
-#    paths = None
-    print(neighbours)
     while paths:
         p = random.choice(paths)
-        #print(p)
         t = [points[i] for i in p]
         j = longest_edge(t)
-        #print(j)
         if j == 0:
             slide_edge(neighbours, (p[0], p[1], p[2]))
         else:
             slide_edge(neighbours, (p[2], p[1], p[0]))
         print(total_length(neighbours, points))
         paths = slideable_2paths(neighbours, points)
-        #paths = None
-    #print(neighbours)
     draw_graph(neighbours, points)
